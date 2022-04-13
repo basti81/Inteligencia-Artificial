57fef76c76ec4032a1e76035542711a29ca03561
basti81 <bastianastudillo79@gmail.com>
2022-04-12 23:22:20 -0400

    Implementacion de mas funciones

--- Differential Evolution/index.py
+++ Differential Evolution/index.py
@@ -4,10 +4,14 @@ import math
 import my_utility as mu
 
 
-#Funcion objetivo
+#Funcion objetivo (Prueba)
 def fun1(x):
     return sum([x[i]**2 for i in range(len(x))])
-              
+
+#Funcion a implementar
+def fun2(x):
+    return sum([10**(6*(i-1)/(len(x)-1)) * x[i]**2 for i in range(1,len(x))])
+
 # --Constant---
 lb=np.array([-5,-5])  # Lowebound
 ub=np.array([5,5])    # UpperBound 
@@ -15,7 +19,7 @@ t = 10                 # Number of generation
 f_mutate = 0.85       # Mutate function
 np = 5                # Tamaño de la poblacion
 p_recom = 0.5         # probabilidad de mutacion
-cost_obj = fun1
+cost_obj = fun2
 
 
 # ------------
